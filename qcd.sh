ea94069bfc0a546aa4966d65199ffadac033b612
dennis.musk <395181939@qq.com>
2015-12-28 16:06:20 +0800

    add files

---
+++ qcd.sh
@@ -0,0 +1,16 @@
+# changed by zengxianbo @Mon Dec 28 14:20:13 CST 2015
+qcd ()
+{
+	local tmp_path=/tmp/.qcd/tmp
+	local tmp_file="$tmp_path/qcd_tmp"
+
+        mkdir -p $tmp_path 
+	[ -x "$tmp_file" ] || touch $tmp_file 
+	chmod 770 $tmp_path
+	QD=$tmp_file
+	export QD
+	/usr/local/sbin/qcd "$@"
+	[ ! `cat $QD` ] || cd "`cat $QD`"
+	rm -f "$QD"
+        unset QD;
+}
