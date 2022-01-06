f80767e7f33eeeef920d6aa3be8637503240c2fa
zengxianbo <zengxianbo2021@outlook.com>
2022-01-06 11:21:52 +0800

    change install to home

--- qcd.sh
+++ qcd.sh
@@ -1,16 +1,15 @@
 # changed by zengxianbo @Mon Dec 28 14:20:13 CST 2015
 qcd ()
 {
-	local tmp_path=/tmp/.qcd/tmp
+	local tmp_path=$HOME/.qcd/tmp
 	local tmp_file="$tmp_path/qcd_tmp"
-
-        mkdir -p $tmp_path 
+    mkdir -p $tmp_path
 	[ -x "$tmp_file" ] || touch $tmp_file 
 	chmod 770 $tmp_path
 	QD=$tmp_file
 	export QD
-	/usr/local/sbin/qcd "$@"
+	$HOME/bin/qcd "$@"
 	[ ! `cat $QD` ] || cd "`cat $QD`"
 	rm -f "$QD"
-        unset QD;
+    unset QD;
 }
